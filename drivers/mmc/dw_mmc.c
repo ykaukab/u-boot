7710a68a4efaa90acd0ac57aa04909b63199e410
Guillaume GARDET <guillaume.gardet@free.fr>
2018-04-09 10:28:26 +0200

    Temp workaround for Chromebook snow to avoid the 'unable to select a mode' error

--- drivers/mmc/dw_mmc.c
+++ drivers/mmc/dw_mmc.c
@@ -508,7 +508,8 @@ void dwmci_setup_cfg(struct mmc_config *cfg, struct dwmci_host *host,
 		cfg->host_caps |= MMC_MODE_4BIT;
 		cfg->host_caps &= ~MMC_MODE_8BIT;
 	}
-	cfg->host_caps |= MMC_MODE_HS | MMC_MODE_HS_52MHz;
+	/* Temp workaround for Chromebook snow to avoid the 'unable to select a mode' error */
+// 	cfg->host_caps |= MMC_MODE_HS | MMC_MODE_HS_52MHz;
 
 	cfg->b_max = CONFIG_SYS_MMC_MAX_BLK_COUNT;
 }
